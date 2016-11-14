8b5965480c28ae5a51cd0e4081307c2dbbe95812
Taylor Sturtz <taylorsturtz.com>
2016-11-14 00:39:26 -0800

    post/get issue with http and not getting callback to work on add function

--- script.js
+++ script.js
@@ -1,254 +1,119 @@
-/**
- * Define all global variables here
- */
+var app = angular.module('studentGradeTable', []);
 
-var averageGrade = 0;
+app.controller('mainCtrl', function (dataService) {
+    this.student = {};
+    this.student_array = [];
 
-/**
- * student_array - global array to hold student objects
- * @type {Array}
- */
-
-var student_array = [];
-
-/**
- * inputIds - id's of the elements that are used to add students
- * @type {string[]}
- */
-
-student_vars = ['#studentName', '#course', '#studentGrade'];
-
-/**
- * applyClickHandlers - After document has loaded, add click handlers
- */
-
-function applyClickHandlers() {
-    /**
-     * addClicked - Event Handler when user clicks the add button
-     */
-    $('.add-btn').click(addStudent);
-    /**
-     * cancelClicked - Event Handler when user clicks the cancel button, should clear out student form
-     */
-    $('.cancel-btn').click(clearAddStudentForm);
-    /**
-     * getDataClicked - Event Handler when user clicks the get server data button
-     */
-    //$('.getdata-btn').click(getServerData);
-    /**
-     * deleteClicked - Event Handler when user clicks the delete button
-     */
-    $('.list-body').on('click', '.delete-btn', function () {
-        var delete_id = $(this).attr('id'); // id of the item
-        var delete_index = $(this).closest('tr').index(); // index of the row item associated with the student array
-        console.log('delete_id', delete_id);
-        console.log('delete_index', delete_index);
-        removeStudent(delete_id, delete_index);
-    });
-}
-
-/**
- * addStudent - creates a student object based on input fields in the form and adds the object to global student array
- *
- * @return undefined
- */
-function addStudent() {
-    if ($(student_vars[0]).val() == '' || $(student_vars[1]).val() == '' || $(student_vars[2]).val() == '') {
-    alert('please enter all information');
-    }
-    else if ($(student_vars[2]).val() < 0 || $(student_vars[2]).val() > 100 || isNaN($(student_vars[2]).val()) == true) {
-    alert('please enter a number between 0 and 100');
-    }
-    else {
-        var data_object = {
-            api_key: 'BmjoMo3MLu',
-            name: $(student_vars[0]).val(),
-            course: $(student_vars[1]).val(),
-            grade: ($(student_vars[2]).val())
-        };
-        $.ajax({
-            data: data_object,
-            dataType: 'json',
-            method: 'post',
-            url: 'https://s-apis.learningfuze.com/sgt/create',
-            success: function (response) {
-                var studentObject = {
-                    name: $(student_vars[0]).val(),
-                    course: $(student_vars[1]).val(),
-                    grade: ($(student_vars[2]).val()),
-                    id: response.new_id
-                };
-                student_array.push(studentObject);
-                console.log('student array', student_array);
-                updateData();
-                clearAddStudentForm();
-            }
-        });
-    }
-    /*if ($(student_vars[0]).val() == '' || $(student_vars[1]).val() == '' || $(student_vars[2]).val() == '') {
-        alert('please enter all information');
-    }
-    else if ($(student_vars[2]).val() < 0 || $(student_vars[2]).val() > 100 || isNaN($(student_vars[2]).val()) == true) {
-        alert('please enter a number between 0 and 100');
-    }
-    else {
-        var studentObject = {
-            name: $(student_vars[0]).val(),
-            course: $(student_vars[1]).val(),
-            grade: Number($(student_vars[2]).val()),
-            id: student_array.length
-        };
-        console.log(studentObject);
-        student_array.push(studentObject);
-        console.log(student_array);
-        updateData();
-        clearAddStudentForm();
-    }*/
-}
-/**
- * clearAddStudentForm - clears out the form values based on inputIds variable
- */
-function clearAddStudentForm() {
-    console.log('clearAddStudentForm clicked');
-    $(student_vars[0]).val('');
-    $(student_vars[1]).val('');
-    $(student_vars[2]).val('');
-    console.log('forms cleared');
-}
-
-/**
- * removeStudent - removes the student which the user has clicked the delete button
- */
-
-function removeStudent(delete_id, delete_index) {
-    var data_object = {
-        api_key: 'BmjoMo3MLu',
-        student_id: delete_id
-    };
-    console.log('data obj is :', data_object);
-    $.ajax({
-        data: data_object,
-        dataType: 'json',
-        method: 'post',
-        url: 'https://s-apis.learningfuze.com/sgt/delete',
-        success: function (response) {
-            student_array.splice(delete_index, 1); // remove from student_array
-            updateData();
-            console.log('student array', student_array);
-            if (student_array.length === 0) {
-                $('.clear-after-entry').show();
-            }
-        }
-    });
-}
-
-/**
- * getServerData - get and parse data from server
- */
-
-function getServerData() {
-    console.log('getServerData clicked');
-    var data_object = {
-        api_key: 'BmjoMo3MLu'
-    };
-    $.ajax({
-        data: data_object,
-        dataType: 'json',
-        method: 'post',
-        url: 'https://s-apis.learningfuze.com/sgt/get',
-        success: function (response) {
-            console.log('response received: ', response);
+    this.getStudents = function () {
+        dataService.get(function (response) {
             var responseData = response.data;
             console.log('response received: ', responseData);
             for (var i = 0; i < responseData.length; i++) {
-                var studentObject = {
+                var student = {
                     name: responseData[i].name,
                     course: responseData[i].course,
                     grade: responseData[i].grade,
                     id: responseData[i].id
                 };
-                student_array.push(studentObject);
+                student_array.push(student);
                 console.log('student array', student_array);
-                updateData();
-                clearAddStudentForm();
             }
-        }
-    });
-}
+            student = {};
+        });
+    };
 
-/**
- * calculateAverage - loop through the global student array and calculate average grade and return that value
- * @returns {number}
- */
-function calculateAverage() {
-    var totalGrades = 0;
-    var averageGrade = 0;
-    var appendGrade = $('.avgGrade').text(averageGrade);
-    if (student_array.length === 0) {
-        averageGrade = 0;
-        $(appendGrade).text(averageGrade.toFixed(2));
-        return averageGrade.toFixed(2);
-    }
-    else {
-        for (var i = 0; i < student_array.length; i++) {
-            totalGrades += student_array[i].grade;
-        }
-        averageGrade = totalGrades / student_array.length;
-        $(appendGrade).text(averageGrade.toFixed(2));
-        return averageGrade.toFixed(2);
-    }
-}
-/**
- * updateData - centralized function to update the average and call student list update
- */
-function updateData() {
-    updateStudentList();
-    calculateAverage();
-}
-/**
- * updateStudentList - loops through global student array and appends each objects data into the student-list-container > list-body
- */
-function updateStudentList() {
-    // If empty, clear the <h2>: 'User Info Unavailable' AND do the same stuff that's in the else statement
-    if (student_array.length === 1) {
-        $('.clear-after-entry').hide();
-        $('.list-body').empty(); // Empty the tbody DOM elements before re-adding everything in the student array
-    }
-    $('.list-body').empty(); // Empty the tbody DOM elements before re-adding everything in the student array
-    for (var i = 0; i < student_array.length; i++) {
-        //student_array[i].id = i;
-        addStudentToDom(student_array[i]);
-    }
-}
-/**
- * addStudentToDom - take in a student object, create html elements from the values and then append the elements
- * into the .student_list tbody
- * @param studentObj
- */
-function addStudentToDom(studentObj) {
-    var s_row = $('<tr>');
-    var s_name = $('<td>').text(studentObj.name);
-    var s_course = $('<td>').text(studentObj.course);
-    var s_grade = $('<td>').text(studentObj.grade);
-    var s_button = $('<button>').addClass('btn btn-danger delete-btn').text('Delete').attr('id', studentObj.id);
-    var s_button_td = $('<td>').append(s_button);
-    $('.list-body').append(s_row);
-    $('.list-body > tr:last').append(s_name).append(s_course).append(s_grade).append(s_button_td);
-}
-/**
- * reset - resets the application to initial state. Global variables reset, DOM get reset to initial load state
- */
-function reset() {
-    student_array = [];
-    averageGrade = 0;
-    $('.list-body').empty();
-    clearAddStudentForm();
-    $('.avgGrade').text('0');
-}
-/**
- * Listen for the document to load and reset the data to the initial state
- */
-$(document).ready(function () {
-    applyClickHandlers();
-    getServerData();
+    this.addStudent = function () {
+        console.log('add clicked');
+        dataService.add(this.student, function (response) {
+            console.log('callback response');
+            var responseData = response.data;
+            var student = {
+                name: student.name,
+                course: student.course,
+                grade: student.grade,
+                id: responseData.id
+            };
+            student_array.push(student);
+            console.log('student array', student_array);
+            student = {};
+        });
+    };
+
+    this.deleteStudent = function () {
+        console.log('delete clicked');
+        dataService.delete(this.student, function (response) {
+            var responseData = response.data;
+            student_array.splice(student);
+        });
+    };
+
 });
+
+app.service('dataService', function ($http) {
+    var dataServiceSelf = this;
+
+    this.get = function () {
+        $http({
+            data: {api_key: 'BmjoMo3MLu'},
+            dataType: 'json',
+            method: 'post',
+            url: 'https://s-apis.learningfuze.com/sgt/get',
+            cache: false
+        })
+            .then(
+                function (callback) {
+                    console.log('got student data');
+                },
+                function (response) {
+                    console.log('failed to GET student data');
+                }
+            );
+    };
+
+    this.add = function (student) {
+        var addData = {
+            api_key: 'BmjoMo3MLu',
+            name: student.name,
+            course: student.course,
+            grade: student.grade
+        };
+        $http({
+            params: addData,
+            dataType: 'json',
+            method: 'get',
+            url: 'https://s-apis.learningfuze.com/sgt/create',
+            cache: false
+        })
+            .then(
+                function (callback) {
+                    console.log('response received');
+                },
+                function (response) {
+                    console.log('failed to ADD student data');
+                }
+            );
+    };
+
+    this.del = function (student) {
+        $http({
+            data: {api_key: 'BmjoMo3MLu', student_id: student.id},
+            dataType: 'json',
+            method: 'post',
+            url: 'https://s-apis.learningfuze.com/sgt/delete',
+            cache: false
+        })
+            .then(
+                function (callback) {
+                    var responseData = response.data;
+                    console.log('response received: ', responseData);
+                },
+                function (response) {
+                    console.log('failed to DELETE student data');
+                }
+            );
+    };
+
+});
+
+//this.student_array.push(this.student);
+//this.student = {};
