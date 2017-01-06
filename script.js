aee1a3066cbad0a1d6d36b03c362a24aa6f77ea9
taylor_sturtz <taylorsturtz@gmail.com>
2017-01-05 20:12:40 -0800

    grade average working

--- script.js
+++ script.js
@@ -1,40 +1,49 @@
 var app = angular.module('studentGradeTable', []);
 
 app.controller('mainCtrl', function (dataService) {
+    var ctrl = this;
+    ctrl.student_array = [];
+    ctrl.avgGrade = 0.00;
+    function getAvg(arr) {
+        var total = 0;
+        for (var i=0; i<arr.length; i++) {
+            total += arr[i].grade;
+        }
+        return total / arr.length;
+    }
+    console.log('student arr: ', ctrl.student_array);
 
-    var ctrlSelf = this;
-
-    ctrlSelf.student_array = [];
-    console.log('student arr: ', ctrlSelf.student_array);
     //call service to get student data
     dataService.get()
         .then(
             function (response) {
-                ctrlSelf.student_array = response;
+                ctrl.student_array = response;
+                ctrl.avgGrade = getAvg(ctrl.student_array);
             },
             function (response) {
                 console.warn(response);
             });
-    console.log('student arr: ', ctrlSelf.student_array);
+    console.log('student arr: ', ctrl.student_array);
 
     //add student object from form inputs
     this.addStudent = function () {
         console.log('add clicked');
-        if (ctrlSelf.student.grade >= 0 && ctrlSelf.student.grade <= 100 && ctrlSelf.student.grade) {
+        if (ctrl.student.grade >= 0 && ctrl.student.grade <= 100 && ctrl.student.grade) {
             dataService.add(this.student)
                 .then(
                     function (response) {
-                        ctrlSelf.student_array = response;
+                        ctrl.student_array = response;
+                        ctrl.avgGrade = getAvg(ctrl.student_array);
                     },
                     function (response) {
                         console.warn(response);
                     });
-            ctrlSelf.student = {};
+            ctrl.student = {};
         } else {
             // TODO: better form validation
             console.warn('please enter a number between 0 and 100');
         }
-        ctrlSelf.student_array = dataService.student_array;
+        ctrl.student_array = dataService.student_array;
     };
 
     //delete student object
@@ -46,25 +55,24 @@ app.controller('mainCtrl', function (dataService) {
         })
             .then(
                 function (response) {
-                    ctrlSelf.student_array = response;
+                    ctrl.student_array = response;
+                    ctrl.avgGrade = getAvg(ctrl.student_array);
                 },
                 function (response) {
                     console.warn(response);
                 });
-        ctrlSelf.student_array = dataService.student_array;
+        ctrl.student_array = dataService.student_array;
     };
 
 });
 
 app.service('dataService', function ($http, $q) {
-    var dataServiceSelf = this;
+    var dataService = this;
     var student = {};
+    dataService.student_array = [];
 
-    dataServiceSelf.student_array = [];
-
-    dataServiceSelf.get = function () {
+    dataService.get = function () {
         var defer = $q.defer();
-
         $http({
             data: $.param({api_key: 'BmjoMo3MLu'}),
             headers: {'Content-Type': 'application/x-www-form-urlencoded'},
@@ -76,9 +84,9 @@ app.service('dataService', function ($http, $q) {
             .then(
                 function (response) {
                     console.log('got student data');
-                    dataServiceSelf.student_array = response.data.data;
-                    console.log('student array', dataServiceSelf.student_array);
-                    defer.resolve(dataServiceSelf.student_array);
+                    dataService.student_array = response.data.data;
+                    console.log('student array', dataService.student_array);
+                    defer.resolve(dataService.student_array);
                 },
                 function (response) {
                     console.log('failed to GET student data');
@@ -88,7 +96,7 @@ app.service('dataService', function ($http, $q) {
         return defer.promise;
     };
 
-    dataServiceSelf.add = function (student) {
+    dataService.add = function (student) {
         console.log('student passed to service is: ', student);
         var defer = $q.defer();
         var addData = {
@@ -111,9 +119,8 @@ app.service('dataService', function ($http, $q) {
                     var responseData = response.data;
                     console.log('response data: ', responseData);
                     student.id = responseData.new_id; // set id property returned from server to student id property
-                    dataServiceSelf.student_array.push(student); // push new student object into array
-                    dataServiceSelf.update(); // returns student array to update view
-                    defer.resolve(dataServiceSelf.student_array);
+                    dataService.student_array.push(student); // push new student object into array
+                    defer.resolve(dataService.student_array);
                 },
                 function (response) {
                     console.log('failed to ADD student data');
@@ -123,7 +130,7 @@ app.service('dataService', function ($http, $q) {
         return defer.promise;
     };
 
-    dataServiceSelf.del = function (student) {
+    dataService.del = function (student) {
         var defer = $q.defer();
         $http({
             data: $.param({api_key: 'BmjoMo3MLu', student_id: student.id}),
@@ -137,11 +144,10 @@ app.service('dataService', function ($http, $q) {
                 function (response) {
                     var responseData = response.data;
                     console.log('response received: ', responseData);
-                    console.log(dataServiceSelf.student_array);
-                    console.warn(dataServiceSelf.student_array.indexOf(student));
-                    dataServiceSelf.student_array.splice(dataServiceSelf.student_array.indexOf(student), 1); // remove student object from array
-                    dataServiceSelf.update(); // returns student array to update view
-                    defer.resolve(dataServiceSelf.student_array);
+                    console.log(dataService.student_array);
+                    console.warn(dataService.student_array.indexOf(student));
+                    dataService.student_array.splice(dataService.student_array.indexOf(student), 1); // remove student object from array
+                    defer.resolve(dataService.student_array);
                 },
                 function (response) {
                     console.log('failed to DELETE student data');
@@ -151,9 +157,9 @@ app.service('dataService', function ($http, $q) {
         return defer.promise;
     };
 
-    dataServiceSelf.update = function () {
-        console.log('update', dataServiceSelf.student_array);
-        return dataServiceSelf.student_array;
+    dataService.update = function () {
+        console.log('update', dataService.student_array);
+        return dataService.student_array;
     }
 
 });
