93b1d809a03fd35b4fce0e474676e98d3510892c
taylor_sturtz <taylorsturtz@gmail.com>
2017-01-06 17:40:23 -0800

    firebase implemented and CRUD ops all working. Need to clean up code still.

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 angular.module('studentGradeTable', [])
 
-    .controller('mainCtrl', function (dataService) {
+    .controller('mainCtrl', function (dataService, $scope) {
         var ctrl = this;
+        var fb = firebase.database();
+
         ctrl.student_array = dataService.student_array;
         ctrl.avgGrade = 0.00;
         function getAvg(arr) {
@@ -30,6 +32,7 @@ angular.module('studentGradeTable', [])
                 dataService.add(ctrl.student)
                     .then(
                         function (response) {
+                            console.log(response);
                             ctrl.student_array = response;
                             ctrl.avgGrade = getAvg(ctrl.student_array);
                         },
@@ -47,6 +50,7 @@ angular.module('studentGradeTable', [])
 
         //delete student object
         this.deleteStudent = function (student) {
+            console.log(student);
             dataService.del(student)
                 .then(
                     function (response) {
@@ -64,15 +68,6 @@ angular.module('studentGradeTable', [])
 
     .service('dataService', function ($http, $q) {
 
-        // Initialize Firebase
-        var config = {
-            apiKey: "AIzaSyC-U6_tmCFDKu20Kmf3dzSaCn340Ze7kQ4",
-            authDomain: "ng-sgt.firebaseapp.com",
-            databaseURL: "https://ng-sgt.firebaseio.com",
-            storageBucket: "ng-sgt.appspot.com",
-            messagingSenderId: "552596427770"
-        };
-        firebase.initializeApp(config);
         var fb = firebase.database();
 
         var dataService = this;
@@ -80,9 +75,13 @@ angular.module('studentGradeTable', [])
 
         dataService.get = function () {
             var defer = $q.defer();
-            fb.ref('students').on('value', function(snapshot) {
-                console.log('snapshot from service: ', snapshot.val());
+            fb.ref('students').on('value', function (snapshot) {
                 dataService.student_array = snapshot.val();
+                snapshot.forEach(function (childSnapshot) {
+                    dataService.student_array[childSnapshot.key].id = childSnapshot.key;
+                });
+                dataService.student_array = Object.values(dataService.student_array); // Convert JSON object to array
+                console.info(dataService.student_array);
                 defer.resolve(dataService.student_array);
             });
             return defer.promise;
@@ -90,56 +89,82 @@ angular.module('studentGradeTable', [])
 
         dataService.add = function (student) {
             var defer = $q.defer();
-            var addData = {
-                api_key: 'BmjoMo3MLu',
-                name: student.name,
-                course: student.course,
-                grade: student.grade
-            };
-            $http({
-                data: $.param(addData),
-                headers: {'Content-Type': 'application/x-www-form-urlencoded'},
-                dataType: 'json',
-                method: 'post',
-                url: 'https://s-apis.learningfuze.com/sgt/create',
-                cache: false
-            })
+
+            fb.ref('students').push(student)
                 .then(
                     function (response) {
-                        student.id = response.data.new_id; // set id property returned from server to student id property
-                        dataService.student_array.push(student); // push new student object into array
+                        //student.id = response.data.new_id; // set id property returned from server to student id property
                         defer.resolve(dataService.student_array);
                     },
-                    function (response) {
+                    function (err) {
                         // TODO: better error handling
-                        console.log('failed to ADD student data');
-                        defer.reject('failed to ADD student data');
-                    }
-                );
+                        console.log('failed to ADD student data', err);
+                        defer.reject('failed to ADD student data', err);
+                    });
+
+            /*var addData = {
+             api_key: 'BmjoMo3MLu',
+             name: student.name,
+             course: student.course,
+             grade: student.grade
+             };
+             $http({
+             data: $.param(addData),
+             headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+             dataType: 'json',
+             method: 'post',
+             url: 'https://s-apis.learningfuze.com/sgt/create',
+             cache: false
+             })
+             .then(
+             function (response) {
+             student.id = response.data.new_id; // set id property returned from server to student id property
+             dataService.student_array.push(student); // push new student object into array
+             defer.resolve(dataService.student_array);
+             },
+             function (response) {
+             // TODO: better error handling
+             console.log('failed to ADD student data');
+             defer.reject('failed to ADD student data');
+             }
+             );*/
             return defer.promise;
         };
 
         dataService.del = function (student) {
             var defer = $q.defer();
-            $http({
-                data: $.param({api_key: 'BmjoMo3MLu', student_id: student.id}),
-                headers: {'Content-Type': 'application/x-www-form-urlencoded'},
-                dataType: 'json',
-                method: 'post',
-                url: 'https://s-apis.learningfuze.com/sgt/delete',
-                cache: false
-            })
+
+            fb.ref('students/' + student.id).remove()
                 .then(
                     function (response) {
-                        dataService.student_array.splice(dataService.student_array.indexOf(student), 1); // remove student object from array
+                        //student.id = response.data.new_id; // set id property returned from server to student id property
                         defer.resolve(dataService.student_array);
                     },
-                    function (response) {
+                    function (err) {
                         // TODO: better error handling
-                        console.log('failed to DELETE student data');
-                        defer.reject('failed to DELETE student data');
-                    }
-                );
+                        console.log('failed to DEL student data', err);
+                        defer.reject('failed to DEL student data', err);
+                    });
+
+            /*$http({
+             data: $.param({api_key: 'BmjoMo3MLu', student_id: student.id}),
+             headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+             dataType: 'json',
+             method: 'post',
+             url: 'https://s-apis.learningfuze.com/sgt/delete',
+             cache: false
+             })
+             .then(
+             function (response) {
+             dataService.student_array.splice(dataService.student_array.indexOf(student), 1); // remove student object from array
+             defer.resolve(dataService.student_array);
+             },
+             function (response) {
+             // TODO: better error handling
+             console.log('failed to DELETE student data');
+             defer.reject('failed to DELETE student data');
+             }
+             );*/
             return defer.promise;
         };
 
