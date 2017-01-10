654c337a5ea89ac49369e6ab6bcd0752c09f25f1
taylor_sturtz <taylorsturtz@gmail.com>
2017-01-09 17:25:04 -0800

    form validation done and code cleanup

--- script.js
+++ script.js
@@ -3,23 +3,26 @@ angular.module('studentGradeTable', ['focus-if', 'ngAnimate'])
     .controller('mainCtrl', function (dataService) {
         var ctrl = this;
 
-        // Set initial and calculate grade average
+        // Calculate grade average
         ctrl.avgGrade = 0.00;
         function getAvg(arr) {
             var total = 0;
             var countOfNaNs = 0;
+            // Check if there is anything to calculate an average on
             if (arr.length === 0) {
                 return 0.00;
             } else {
                 for (var i = 0; i < arr.length; i++) {
+                    // Check if grade is NaN to omit it from grade average calculation
                     if (isNaN(arr[i].grade)) {
                         countOfNaNs++;
-                        console.log('student removed from grade average calculation because of NaN grade property: ', arr[i].name)
                     } else {
                         total += arr[i].grade;
                     }
                 }
                 var result = total / (arr.length - countOfNaNs);
+                // Assign color-scale classes based on grade average calculation result
+                // Less than 70 is red, 70-80 is orange, 80-90 is blue, over 90 is green.
                 ctrl.avgGradeColor = result === 'No grade' ? 'emptyClass'
                     : result >= 90 ? 'label-success'
                     : result >= 80 ? 'label-info'
@@ -30,8 +33,7 @@ angular.module('studentGradeTable', ['focus-if', 'ngAnimate'])
             }
         }
 
-        // Set color scale for grade average.
-        // 'No grade' applies emptyClass styling.
+        // Set color scale for students' grades
         // Less than 70 is red, 70-80 is orange, 80-90 is blue, over 90 is green.
         ctrl.getGradeColor = function (grade) {
             ctrl.gradeColor = grade === 'No grade' ? 'emptyClass'
@@ -43,84 +45,66 @@ angular.module('studentGradeTable', ['focus-if', 'ngAnimate'])
             return ctrl.gradeColor;
         };
 
-        // Set initial sort order and logic for orderBy param clicked
+        // Set initial sort order and logic for orderBy param clicked to sort by Name, Course, or Grade
         ctrl.order = '-grade';
         ctrl.orderItems = function (orderBy) {
             ctrl.order = orderBy === ctrl.ordered ? '-' + orderBy : orderBy;
             ctrl.ordered = ctrl.order;
         };
 
-        // Call service to get student data, then sync array and get grade average (updates DOM).
+        // Call service to get student data, then sync array and get grade average and update DOM
         dataService.get()
             .then(
                 function (response) {
                     ctrl.student_array = response;
                     ctrl.avgGrade = getAvg(ctrl.student_array);
-                },
-                function (err) {
-                    // TODO: better error handling
-                    console.warn(err);
                 });
 
-        // Pass student object (from form inputs) to service to be added, then sync array and get grade average (updates DOM).
-        ctrl.addStudent = function () {
-            if (ctrl.student.grade === 0 || ctrl.student.grade > 0 && ctrl.student.grade <= 100 && ctrl.student.grade) {
-                dataService.add(ctrl.student)
-                    .then(
-                        function (response) {
-                            ctrl.student_array = response;
-                            ctrl.avgGrade = getAvg(ctrl.student_array);
-                        },
-                        function (err) {
-                            // TODO: better error handling
-                            console.warn(err);
-                        });
-                // Clear form inputs
-                ctrl.student = {};
-            } else {
-                // TODO: better form validation
-                console.warn('you entered ' + '"' + ctrl.student.grade + '"' + ' -- please enter a number between 0 and 100, inclusive');
+        // Pass student object (from form inputs) to service to be added, then sync array, get grade average (updates DOM)
+        ctrl.addStudent = function (student) {
+            if (student !== undefined) {
+                if (student.name && student.course && student.grade) {
+                    dataService.add(student)
+                        .then(
+                            function (response) {
+                                ctrl.student_array = response;
+                                ctrl.avgGrade = getAvg(ctrl.student_array);
+                            });
+                    // Clear form inputs
+                    ctrl.student = {};
+                }
             }
         };
 
-        // Pass student object to service to be deleted, then sync array and get grade average (updates DOM).
+        // Pass student object to service to be deleted, then sync array and get grade average (updates DOM)
         ctrl.deleteStudent = function (student) {
             dataService.del(student)
                 .then(
                     function (response) {
                         ctrl.student_array = response;
                         ctrl.avgGrade = getAvg(ctrl.student_array);
-                    },
-                    function (err) {
-                        // TODO: better error handling
-                        console.warn(err);
                     });
         };
 
+        // Pass student object to service to be edited, then sync array and get grade average (updates DOM)
         ctrl.editStudent = function (student, prop, update) {
-            console.log('Student Object: ', student);
-            console.log('property being updated: ', prop);
-            console.log('updating with: ', update);
+            // Check if the update is an empty string
             if (update === null || update === '') {
                 update = prop === 'name' ? 'No name'
                     : prop === 'course' ? 'No course'
                     : 'No grade'
             }
+            // Check if there is actually an update, if not, exit the function
             if (update === undefined) {
-                console.log('Not editing anything');
                 return;
             }
+            // Rewrite the edited property with the updated value
             student[prop] = update;
-            console.info('Student Object: ', student);
             dataService.edit(student)
                 .then(
                     function (response) {
                         ctrl.student_array = response;
                         ctrl.avgGrade = getAvg(ctrl.student_array);
-                    },
-                    function (err) {
-                        // TODO: better error handling
-                        console.warn(err);
                     });
         }
 
@@ -137,12 +121,13 @@ angular.module('studentGradeTable', ['focus-if', 'ngAnimate'])
             messagingSenderId: "552596427770"
         };
         firebase.initializeApp(config);
+        // Create reference to Firebase
         var fb = firebase.database();
 
         var dataService = this;
         dataService.student_array = [];
 
-        // Gets students array from Firebase and returns a promise.
+        // Gets students array from Firebase and returns a promise
         dataService.get = function () {
             var defer = $q.defer();
             fb.ref('students').on('value', function (snapshot) {
@@ -163,23 +148,19 @@ angular.module('studentGradeTable', ['focus-if', 'ngAnimate'])
             return defer.promise;
         };
 
-        // Adds student object as a value of a unique key, generated based on timestamp. Pushes it to the last position on Firebase 'students' JSON object and then returns a promise to controller.
+        // Adds student object as a value of a unique key, generated based on timestamp
+        // Pushes it to the last position on Firebase 'students' JSON object and then returns a promise to controller
         dataService.add = function (student) {
             var defer = $q.defer();
             fb.ref('students').push(student)
                 .then(
                     function () {
                         defer.resolve(dataService.student_array);
-                    },
-                    function () {
-                        // TODO: better error handling
-                        console.log('failed to ADD student data');
-                        defer.reject('failed to ADD student data');
                     });
             return defer.promise;
         };
 
-        // Deletes student object from Firebase based on the object's key which is identical to its 'id' property and then returns a promise to controller.
+        // Deletes student object from Firebase based on the object's key which is identical to its 'id' property and then returns a promise to controller
         dataService.del = function (student) {
             var defer = $q.defer();
             fb.ref('students/' + student.id).remove()
@@ -192,33 +173,23 @@ angular.module('studentGradeTable', ['focus-if', 'ngAnimate'])
                             }
                         });
                         defer.resolve(dataService.student_array);
-                    },
-                    function () {
-                        // TODO: better error handling
-                        console.log('failed to DEL student data');
-                        defer.reject('failed to DEL student data');
                     });
             return defer.promise;
         };
 
-        // Updates student object in Firebase based on the object's key which is identical to its 'id' property and then returns a promise to controller.
+        // Updates student object in Firebase based on the object's key which is identical to its 'id' property and then returns a promise to controller
         dataService.edit = function (student) {
             var defer = $q.defer();
-            // Delete $$hashKey property from student object before sending to Firebase.
+            // Delete $$hashKey property from student object before sending to Firebase
             var hashKeyProperty = ['$$hashKey'];
             delete student[hashKeyProperty];
-            // Use updateObj to pass key reference to Firebase and avoid object property inception.
+            // Use updateObj to pass key reference to Firebase and avoid object property inception
             var updateObj = {};
             updateObj['students/' + student.id] = student;
             fb.ref().update(updateObj)
                 .then(
                     function () {
                         defer.resolve(dataService.student_array);
-                    },
-                    function () {
-                        // TODO: better error handling
-                        console.log('failed to EDIT student data');
-                        defer.reject('failed to EDIT student data');
                     });
             return defer.promise;
         };
@@ -233,4 +204,26 @@ angular.module('studentGradeTable', ['focus-if', 'ngAnimate'])
                 $(element).tooltip({delay: {show: 1000, hide: 500}, animation: true});
             }
         };
+    })
+
+    // Create Bootstrap popover directive for form validation
+    .directive('popover', function ($timeout) {
+        return {
+            restrict: 'A',
+            link: function (scope, element) {
+                $(element).click(function() {
+                    // Check if any of the input values are empty
+                    if ($(element["0"].offsetParent.children[2]["0"]).val().length === 0 ||
+                        $(element["0"].offsetParent.children[2]["1"]).val().length === 0 ||
+                        $(element["0"].offsetParent.children[2]["2"]).val().length === 0)
+                    {
+                        $(element).popover('show');
+                        $timeout(function () {
+                            $(element).popover('hide');
+                        }, 3000);
+                    }
+                });
+            }
+        };
+
     });
